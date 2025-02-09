8a510d93b820f1dbc4c03180705532023d372dc1
livnatcohen <livnatc@nanobebe.com>
2025-02-09 19:38:24 +0200

    initual

---
+++ src/data_loader.py
@@ -0,0 +1,9 @@
+import yfinance as yf
+import pandas as pd
+
+def fetch_stock_data(ticker, start='2015-01-01', end='2024-01-01'):
+    data = yf.download(ticker, start=start, end=end)
+    return data[['Open', 'High', 'Low', 'Close', 'Volume']]
+
+data = fetch_stock_data('AAPL')
+data.to_csv('data\AAPL.csv')
